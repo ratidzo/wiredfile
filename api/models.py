cfb691b7310bd30f24555e8acca85f9454fcd7f7
ratidzo <ratygore@gmail.com>
2024-01-22 22:29:21 +0200

    dev: create model class for school

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class School(models.Model):
+    owner = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="school"
+    )
+    name = models.CharField(max_length=50)
+    date_created = models.DateTimeField(auto_now_add=True)
